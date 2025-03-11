023aa3f6725a8a376cc552294368bb3da6f72a9f
ArrayPowerPlay <160387926+ArrayPowerPlay@users.noreply.github.com>
2025-03-11 20:49:39 +0700

    Update Wed.cpp

--- Week_1/Wed.cpp
+++ Week_1/Wed.cpp
@@ -6,7 +6,9 @@ public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
         int res = 0;
         int len = colors.size();
+        // this vector contains lengths of all alternating groups in 'colors'
         vector<int> lenGroup;
+        // calculate length of each alternating group
         int maxLen = 1;
 
         for (int i = 1; i < len; i++) {
@@ -17,10 +19,13 @@ public:
                 maxLen = 1;
             }
         }
+
         bool check = colors[0] != colors[len - 1];
+        // edge case: 1 0 1 0 1 0
         if (lenGroup.empty() && check) {
             res = (maxLen >= k) ? len : 0;
             return res;
+        // if first element = last element, we need to sum two lengths of two alternating groups
         } else if (check) {
             lenGroup[0] += maxLen;
         } else {
@@ -34,4 +39,6 @@ public:
         return res;
     }
 };
+
+
 
